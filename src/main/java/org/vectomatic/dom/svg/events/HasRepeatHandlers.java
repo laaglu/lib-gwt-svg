f0a27016334cd08b671aaa88ab835989c040fdcb
laaglu <laaglu@d5b21e57-afc5-4760-a3f5-3efab009ca8d>
2010-03-16 22:40:11 +0000

    Project renaming for SEO

--- src/main/java/org/vectomatic/dom/svg/events/HasRepeatHandlers.java
+++ src/main/java/org/vectomatic/dom/svg/events/HasRepeatHandlers.java
@@ -1,6 +1,6 @@
 /**********************************************
  * Copyright (C) 2010 Lukas Laag
- * This file is part of libgwtsvg.
+ * This file is part of lib-gwt-svg.
  * 
  * libgwtsvg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
--- src/test/java/org/vectomatic/libgwtsvg/AppTest.java
+++ src/test/java/org/vectomatic/libgwtsvg/AppTest.java
@@ -1,6 +1,6 @@
 /**********************************************
  * Copyright (C) 2009 Lukas Laag
- * This file is part of libgwtsvg.
+ * This file is part of lib-gwt-svg.
  * 
  * libgwtsvg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
