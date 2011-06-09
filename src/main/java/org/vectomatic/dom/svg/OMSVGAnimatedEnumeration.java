b5b3101b4b9aae7934be8aac10e4721d7209eee9
laaglu <laaglu@d5b21e57-afc5-4760-a3f5-3efab009ca8d>
2011-06-09 09:34:37 +0000

    Added constants to OMSVGAnimatedEnumeration Fixed copyleft in SVGPaintParser

--- src/main/java/org/vectomatic/dom/svg/OMSVGAnimatedEnumeration.java
+++ src/main/java/org/vectomatic/dom/svg/OMSVGAnimatedEnumeration.java
@@ -37,6 +37,23 @@ import com.google.gwt.core.client.JavaScriptObject;
  * and which can be animated.
  */
 public class OMSVGAnimatedEnumeration extends JavaScriptObject {
+  /**
+   * The type is not one of predefined types. It is invalid to attempt to
+   * define a new value of this type or to attempt to switch an existing
+   * value to this type.
+   */
+  public static final short SVG_UNIT_TYPE_UNKNOWN = 0;
+
+  /**
+   * Corresponds to value <span class='attr-value'>'userSpaceOnUse'</span>.
+   */
+  public static final short SVG_UNIT_TYPE_USERSPACEONUSE = 1;
+
+  /**
+   * Corresponds to value <span class='attr-value'>'objectBoundingBox'</span>.
+   */
+  public static final short SVG_UNIT_TYPE_OBJECTBOUNDINGBOX = 2;
+  
   protected OMSVGAnimatedEnumeration() {
   }
 
